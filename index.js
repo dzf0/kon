b5bdf7012360bcd5f696de22afe01246292c617e
Aashir Abdullah <aashiraashir66@gmail.com>
2025-11-23 12:47:27 +0530

    aefaafae

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { Client, GatewayIntentBits, Collection, EmbedBuilder } = require('discord.js');
-const keydrop = require('.commands/keydrop.js'); // Import keydrop module
+const keydrop = require('./commands/keydrop.js'); // Import keydrop module
 
 // Start Express server to keep bot awake on some hosts
 const app = express();
