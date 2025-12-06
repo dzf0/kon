753ad80927cbc10b9d73ed07e26c63c893557a15
Aashir Abdullah <aashiraashir66@gmail.com>
2025-12-07 00:20:23 +0530

    asd

--- commands/dice.js
+++ commands/dice.js
@@ -10,7 +10,6 @@ module.exports = {
       return message.channel.send('Usage: `.dice <amount>` (bet must be positive number)');
     }
 
-    // userData is already loaded from MongoDB by index.js
     if (typeof userData.balance !== 'number') userData.balance = 0;
 
     if (userData.balance < bet) {
@@ -25,26 +24,25 @@ module.exports = {
     let result = '';
 
     if (roll === 6) {
-      reward = bet * 5;
+      // High roll, but not crazy reward
+      reward = Math.floor(bet * 3);
       userData.balance += reward;
-      result = `🎲 You rolled **6**! Jackpot, you win **${reward}** (5x your bet)!`;
-    } else if (roll >= 4) {
-      reward = bet * 2;
+      result = `🎲 You rolled **6**! You win **${reward}** (3x your bet)!`;
+    } else if (roll === 4 || roll === 5) {
+      reward = Math.floor(bet * 1.5);
       userData.balance += reward;
-      result = `🎲 You rolled **${roll}**! Nice, you win **${reward}** (2x your bet)!`;
+      result = `🎲 You rolled **${roll}**! You win **${reward}** (1.5x your bet)!`;
     } else {
       result = `🎲 You rolled **${roll}**. Unlucky, you lose your bet.`;
     }
 
-    // Persist to MongoDB
+    // Persist to MongoDB (only changed fields)
     await saveUserData({ balance: userData.balance });
 
     const embed = new EmbedBuilder()
       .setTitle('Dice Roll')
       .setDescription(result)
-      .addFields(
-        { name: 'New Balance', value: userData.balance.toString(), inline: true }
-      )
+      .addFields({ name: 'New Balance', value: userData.balance.toString(), inline: true })
       .setColor(reward > 0 ? '#00FF00' : '#FF0000')
       .setTimestamp();
 
