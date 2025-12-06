062f5d3f43097588cf19794a5428204447296754
Aashir Abdullah <aashiraashir66@gmail.com>
2025-12-07 00:45:23 +0530

    asd

--- commands/keydrop.js
+++ commands/keydrop.js
@@ -47,7 +47,7 @@ async function handleKeyDrop(message, client) {
   }
 
   // 5% chance per message to spawn a new key if none active
-  if (!currentKey && Math.random() <= 0.10) {
+  if (!currentKey && Math.random() <= 0.05) {
     const rarity = getRandomRarity();
     currentKey = {
       rarity, // string like "Legendary"
